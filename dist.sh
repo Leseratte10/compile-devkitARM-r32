caadb7f30847983a1620475f8f538798ab249182
Florian Bach <leseratte10@vodafone.de>
2020-02-18 21:26:45 +0100

    Fix compiling on Ubuntu

--- dist.sh
+++ dist.sh
@@ -2,3 +2,6 @@
 
 cp /opt/devkitARM-r32-linux_debian-squeeze.tar.gz /github/workspace || /bin/true
 cp /opt/devkitARM-r32-linux_debian-buster.tar.gz /github/workspace || /bin/true
+cp /opt/devkitARM-r32-linux_ubuntu-focal.tar.gz /github/workspace || /bin/true
+cp /opt/devkitARM-r32-linux_ubuntu-bionic.tar.gz /github/workspace || /bin/true
+
--- fix-compile-errors.sh
+++ fix-compile-errors.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-echo "NO WERROR CRAP"
+echo "*******************************************"
+echo "This old version of GCC has a bunch of stuff"
+echo "that doesn't compile with new GCC versions. "
+echo ""
 
+echo -n "NO WERROR ... "
 find -name "configure" -print0 | xargs -0 sed -i 's#GCC_WARN_CFLAGS="$GCC_WARN_CFLAGS -Werror"##g'
 find -name "warning.m4" -print0 | xargs -0 sed -i 's#GCC_WARN_CFLAGS="$GCC_WARN_CFLAGS -Werror"##g'
+echo "done"
 
-echo "FIX GCC PATCH"
+echo -n "FIX GCC PATCH ... "
 chown root:root "gcc-4.5.1" -R
 cd "gcc-4.5.1"
 patch -p1 < ../0001-patch-gcc-fix-gnu-inline.patch
+echo "done"
 
-echo "FIX / REMOVE TEXI FILES"
+echo -n "FIX / REMOVE TEXI FILES ... "
 find -name "*.texi" -print0 | xargs -0 sed -i 's#@itemx#@item#g'
 echo > ./gcc/doc/gcc.texi
-
 cd ..
 
-echo "DONE"
-sleep 1
+echo "done"
+echo "Patching done"
+echo "*******************************************"
 
