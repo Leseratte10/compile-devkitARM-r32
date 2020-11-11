bc060667db2c7bd4e1e379eef374a470ce8e28a6
Florian Bach <leseratte10@vodafone.de>
2020-11-11 13:30:26 +0100

    Fix groovy on newest gcc

--- Dockerfiles/ubuntugroovy/fix-compile-errors.sh
+++ Dockerfiles/ubuntugroovy/fix-compile-errors.sh
@@ -10,6 +10,12 @@ find -name "configure" -print0 | xargs -0 sed -i 's#GCC_WARN_CFLAGS="$GCC_WARN_C
 find -name "warning.m4" -print0 | xargs -0 sed -i 's#GCC_WARN_CFLAGS="$GCC_WARN_CFLAGS -Werror"##g'
 echo "done"
 
+echo -n "COMMON ... "
+# https://wiki.gentoo.org/wiki/Gcc_10_porting_notes/fno_common 
+find -name "configure" -print0 | xargs -0 sed -i 's#GCC_WARN_CFLAGS="-W -Wall -Wstrict-prototypes -Wmissing-prototypes"#GCC_WARN_CFLAGS="-W -Wall -Wstrict-prototypes -Wmissing-prototypes -fcommon"#g'
+find -name "Makefile.in" -print0 | xargs -0 sed -i 's#SIM_EXTRA_CFLAGS = -DMODET -DNEED_UI_LOOP_HOOK -DSIM_TARGET_SWITCHES#SIM_EXTRA_CFLAGS = -DMODET -DNEED_UI_LOOP_HOOK -DSIM_TARGET_SWITCHES -fcommon#g'
+echo "done"
+
 echo -n "FIX GCC PATCH ... "
 chown root:root "gcc-4.5.1" -R
 cd "gcc-4.5.1"
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ devkitARM r32 is needed for lots of ARM applications, including compiling new cI
 The original binaries for devkitARM r32 cannot be downloaded anymore 
 (except for the Windows build from my archive at https://wii.leseratte10.de), 
 thus I am providing these Dockerfiles that run on Debian squeeze, Debian buster, 
-Ubuntu Bionic and Ubuntu Focal (and probably on other versions, too, if you modify them a bit). 
+Ubuntu Bionic, Ubuntu Focal and Ubuntu Groovy (and probably on other versions, too, if you modify them a bit). 
 The Dockerfiles then compile a working devkitARM r32 for that particular distribution. 
 
 Check out this [wiki page](https://github.com/Leseratte10/compile-devkitARM-r32/wiki/Compiling) if you want to compile devkitARM r32 yourself. 
