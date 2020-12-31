9ccd37cd617bd510c0772e69fe9daac5e03ffcd3
Florian Bach <Leseratte10@vodafone.de>
2020-12-31 16:55:29 +0100

    Add more Debian & Ubuntu versions

---
+++ Dockerfiles/debianjessie/dist.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp /opt/devkitARM-r32-linux_debian-jessie.tar.gz /github/workspace || /bin/true
+
---
+++ Dockerfiles/debianstretch/dist.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp /opt/devkitARM-r32-linux_debian-stretch.tar.gz /github/workspace || /bin/true
+
